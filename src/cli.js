b70090f16d951161594f7d8aebfa3c49e2c35fcb
anitahitouch <anitahitouch@gmail.com>
2015-07-07 23:43:14 +0530

    FIX:Handling of Standalone Option

--- src/cli.js
+++ src/cli.js
@@ -23,13 +23,12 @@ node cli.js --listen=8090 --route=window.io
 */
 
 
-var cli = require('minimist');
+var  cli = require('minimist');
 var  FoldersHttp = require('folders-http');	
 var  forwardingProxy = require('folders-http/src/forwardingProxy');
-var  standaloneProxy = require('folders-http/src/standaloneProxy');
-var Server = require('folders-http/src/standaloneServer');
+var  Server = require('folders-http/src/standaloneServer');
 var  Fio = require('folders');
-//var server	= require('folders-http/server.js');
+
 
 var cliHandler = function(){
 	var argv = cli(process.argv.slice(2));
@@ -43,12 +42,11 @@ var cliHandler = function(){
 			break;
 
 		case (module == 'standalone'):
+		case (module == 'server'): 
+
 			standaloneFriendly(argv);
 			break;
-
-		case (module == 'server'): 
-			serverFriendly(argv);
-			break;
+		
 		case (module == 'folders-http'):
 			httpFriendly(argv);
 			break;	
@@ -60,7 +58,6 @@ var cliHandler = function(){
 };
 
 
-
 var forwardFriendly = function(argv){
 	
 	if ('provider' in argv){
@@ -70,19 +67,22 @@ var forwardFriendly = function(argv){
 		options.shareId  = argv['shareid'] = t[1];
 		var f = new forwardingProxy(argv);
 		f.startProxy();
-		// no need to use provider here 
-		// provider can be used in standalone server 
-		//var p = new provider(options,f);
-		//p.fioHandler();
-		
+	
 	}else{
 		var forward = new forwardingProxy(argv)
 		forward.startProxy()
 	}
 };
 
+
+
 var standaloneFriendly = function(argv){
     
+	serverFriendly(argv);
+	
+	 // FIXME:delete below part in future 
+		
+	/*
 	argv = argv || {};
 	if ('provider' in argv){
 		var options = {}
@@ -106,31 +106,36 @@ var standaloneFriendly = function(argv){
 		var routeHandler = new Fio.router(fio);
 		standalone.startProxy(routeHandler, backend);
 	}	
+	*/
 
 };
 
 var serverFriendly = function(argv){
 	argv = argv || {};
-	argv['client'] = argv['_'][1] || '/client.folders.io';
+	argv['client'] = argv['_'][1] ;
 	argv['compress'] = argv['compress'] || 'true' ;
 	argv['log'] = argv['log'] || 'true' ;
 	argv['listen'] = argv['listen'] || 8090 ;
+	argv['mode'] = argv['mode'] || 'DEBUG' ; 
 	
-	if ('provider' in argv){
-		var options = {}
-		var t = argv['provider'].split(':');
-		options.provider = t[0];
-		options.shareId  = t[1];
-		argv['mode'] = 1 ;
-		var s = new Server(argv);
-		// FIXME: Provider pattern is still a bit broken.
-		var p = Fio.provider('local');
-		p = new p(options,s);
-		p.fioHandler();
+	if (argv['mode'] == 'LIVE'){
+		
+		// live or provider mode not correctly implemented yet 
+		
+		argv['backend'] = argv['backend'] || 'local' ;
+
+
+		// using 'local' for backend now.neglecting backend 
+		var Local = Fio.local();
+
+
+		var backend = new Local();
+		var server = new Server(argv,backend);
+		
 	}
 	else{
-		
-		argv['mode'] = argv['mode'] || 'DEBUG';
+
+
 		var fio = new Fio();
 		var backend = new (Fio.stub());
 		var server = new Server(argv,backend);
