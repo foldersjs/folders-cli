2c6155ba1978d3794df33d06349c3906ceadff1c
anitahitouch <anitahitouch@gmail.com>
2015-09-05 13:59:20 +0530

    Implemented cd ls and pwd command

--- src/cli.js
+++ src/cli.js
@@ -21,174 +21,73 @@ node cli.js --route=window.io
 * Listen on port 8090 forwarding requests to window.io as appropriate
 node cli.js --listen=8090 --route=window.io
 */
-
-
-var	 cli = require('minimist');
-var	 FoldersHttp = require('folders-http');	
+var cli = require('minimist');
+var FoldersHttp = require('folders-http');
 //var	 forwardingProxy = require('folders-http/src/forwardingProxy');
-var	 Server = require('folders-http/src/standaloneServer');
-var	 Fio = require('folders');
+var Server = require('folders-http/src/standaloneServer');
+var Fio = require('folders');
 //Used to wrap a provider in Nodejs-compatible mode!
 var FolderFs = require('folders/src/fs');
 var Ftp = require('folders-ftp');
 
-var cliHandler = function(){
-	var argv = cli(process.argv.slice(2));
-	var module = argv['_'][0];
-	
-	switch(true){
-		
+var Cli = function (argv) {
+    var self = this;
+    self.currentDirectory = '/';
+    argv = argv || cli(process.argv.slice(2));
+    var module = argv['_'][0];
 
-		case (module == 'forward'):
-			forwardFriendly(argv)
-			break;
+    switch (true) {
 
-		case (module == 'standalone'):
-		case (module == 'server'): 
 
-			standaloneFriendly(argv);
-			break;
-		
-		case (module == 'folders-http'):
-			httpFriendly(argv);
-			break;	
-		default:
-			// nothing we can do.  Just hope for the best.
-			return
-	}
-	
+    case (module == 'forward'):
+        self.forwardFriendly(argv)
+        break;
+
+    case (module == 'standalone'):
+    case (module == 'server'):
+
+        self.standaloneFriendly(argv);
+        break;
+
+    case (module == 'folders-http'):
+        self.httpFriendly(argv);
+        break;
+    default:
+        // no module specified .trying to directly mount provider
+        self.providerFriendly(argv);
+        return
+    }
+
 };
 
+Cli.prototype.standaloneFriendly = function (argv) {
 
-var forwardFriendly = function(argv){
-	
-	if ('provider' in argv){
-		var options = {}
-		var t = argv['provider'].split(':');
-		options.provider = t[0];
-		options.shareId	 = argv['shareid'] = t[1];
-		var f = new forwardingProxy(argv);
-		f.startProxy();
-	
-	}else{
-		var forward = new forwardingProxy(argv)
-		forward.startProxy()
-	}
+    var self = this;
+
+    self.serverFriendly(argv);
+
 };
 
+Cli.prototype.serverFriendly = function (argv) {
+    var self = this;
+    argv = argv || {};
+    argv['client'] = argv['client'] || argv['_'][1] || false;
+    argv['compress'] = argv['compress'] || false;
+    argv['log'] = argv['log'] || false;
+    argv['listen'] = argv['listen'] || 8090;
+    argv['mode'] = argv['mode'] || 'DEBUG';
+
+    if (argv['mode'] == 'DEBUG') {
+
+        self.providerFriendly(argv);
+
+        var server = new Server(argv, self.serverbackend);
 
 
-var standaloneFriendly = function(argv){
-	
-	serverFriendly(argv);
-	
-	 
-};
+    }
 
 
-function configureFtp(Config, file){
-
-	
-	var ftp_options = {} ;
-	file = file || 'ftp.json' ;
-	Config =  Config || require("../" + file); 
-	var backend  ;
-	
-	if (Config.backend.provider === 'aws'){
-	    var aws_options = configureAws(Config.backend.options);
-		backend = new FolderFs(Fio.provider('aws', aws_options).create('aws'))
-		
-	}
-	
-	// code for other backend synthetic file systems may come here 
-	
-	ftp_options.connectionString = Config.connectionString;
-	ftp_options.enableEmbeddedServer = Config.enableEmbeddedServer;
-	ftp_options.backend = backend ;
-	return ftp_options ;
-	
-};
-
-function configureAws(Config,file){
-	file = file || 'aws.json' ;
-    Config = Config || require("../" + file); 
-	var aws_options = {} ;
-	aws_options.accessKeyId = Config.accessKeyId;
-	aws_options.secretAccessKey = Config.secretAccessKey;
-	aws_options.service = Config.service;
-	aws_options.region =  Config.region;
-	aws_options.bucket =  Config.bucket	;
-	return aws_options ;
-	
-	
-};
-
-
-function configureSsh(Config, file ){
-
-	
-	var ssh_options = {} ;
-	file = file || 'ssh.json' ;
-	Config = Config || require("../" + file );
-		
-	var backend  ;
-	
-	if (Config.backend.provider === 'aws'){
-	    var aws_options = configureAws(Config.backend.options);
-		backend = Fio.provider('aws', aws_options).create('aws')
-		
-	}
-	
-	ssh_options.connectionString = Config.connectionString;
-	ssh_options.enableEmbeddedServer = Config.enableEmbeddedServer;
-	ssh_options.backend = backend ;
-	return ssh_options ;
-	
-};
-
-
-var serverFriendly = function(argv){
-	argv = argv || {};
-	argv['client'] = argv['_'][1] ;
-	argv['compress'] = argv['compress'] || 'true' ;
-	argv['log'] = argv['log'] || 'true' ;
-	argv['listen'] = argv['listen'] || 8090 ;
-	argv['mode'] = argv['mode'] || 'DEBUG' ; 
-	
-
-	if ('provider' in argv) {
-		
-		console.log('provider specified');
-		var options = {};
-		var serverbackend ;
-		var t = argv['provider'].split(':');
-
-		var provider = t[0];
-		var shareId  = t[1];
-		
-		argv['mode'] = 'DEBUG' ;
-		
-
-		if (provider === 'ftp'){
-			var ftp_options = configureFtp(null,argv['ftp-config-file']); 
-			serverbackend = Fio.provider(provider, ftp_options).create('prefix');
-		}
-		
-		if (provider === 'ssh'){
-			var ssh_options = configureSsh(null,argv['ssh-config-file']); 
-			serverbackend = Fio.provider(provider, ssh_options).create('prefix');
-		}
-		
-		if (provider === 'aws'){
-			var aws_options = configureAws(null,argv['aws-config-file']); 
-			serverbackend = Fio.provider(provider, aws_options).create('prefix');
-		}
-		
-		 //?What is prefix!?
-		
-		//console.log('backend: ', backend);
-		var server = new Server(argv, serverbackend);
-		/*
+    /*
 		if (argv['mode'] == 'LIVE'){
 			
 			// live or provider mode not correctly implemented yet 
@@ -202,34 +101,243 @@ var serverFriendly = function(argv){
 	
 			var backend = new Local();
 			var server = new Server(argv,backend);
-		*/		
-
-	}
-	else{
+		*/
 
 
-		var fio = new Fio();
-		var backend = new (Fio.stub());
-		console.log('serverFriendly, backend: ', backend);
-		var server = new Server(argv,backend);
-	}	
+
 };
 
-var httpFriendly = function(argv){
-	
-	if ('provider' in argv){
-		var options = {};
-		var provider = argv['provider'];
-		var options = {};
-		var provider = argv['provider'] || 'local';
-		options.prefix = argv['prefix'];
-		options.host = argv['host'] || 'http://folders.io';
-		options.cb = function(err){console.log(err);}
-		options.provider = Fio.provider(provider).create(prefix);
-		new FoldersHttp(options);
-		
-	}
-	
+Cli.prototype.httpFriendly = function (argv) {
+
+    if ('provider' in argv) {
+        var options = {};
+        var provider = argv['provider'];
+        var options = {};
+        var provider = argv['provider'] || 'local';
+        options.prefix = argv['prefix'];
+        options.host = argv['host'] || 'http://folders.io';
+        options.cb = function (err) {
+            console.log(err);
+        }
+        options.provider = Fio.provider(provider).create(prefix);
+        new FoldersHttp(options);
+
+    }
+
+};
+
+
+
+
+Cli.prototype.forwardFriendly = function (argv) {
+
+    if ('provider' in argv) {
+        var options = {}
+        var t = argv['provider'].split(':');
+        options.provider = t[0];
+        options.shareId = argv['shareid'] = t[1];
+        var f = new forwardingProxy(argv);
+        f.startProxy();
+
+    } else {
+        var forward = new forwardingProxy(argv)
+        forward.startProxy()
+    }
+};
+
+Cli.prototype.cd = function (path) {
+
+    var self = this;
+    if (!self.serverbackend) {
+
+        return cb(new Error("Error! File system not mounted"));
+
+    }
+    path = path || '/';
+
+    path = require('path').normalize(path);
+
+    //FIXME: not using backend.stat to check target
+
+    if (path == '/' || path == '~') {
+
+        // change to root directory
+        self.currentDirectory = '/';
+    } else if (path[0] != '/') {
+
+        // path is relative to current directory
+        // append this path to current directory and normalize it 
+        self.currentDirectory = require('path').join(self.currentDirectory, path);
+
+    } else if (path[0] == '/') {
+        self.currentDirectory = require('path').resolve('/', require('path').normalize(path));
+
+    }
+
+
+
+
+};
+
+Cli.prototype.ls = function (path, cb) {
+
+    var self = this,
+        lsPath;
+
+    if (!self.serverbackend) {
+
+        return cb(new Error("Error! File system not mounted"));
+
+    }
+
+    path = path || self.currentDirectory;
+
+    path = require('path').normalize(path);
+
+    lsPath = path;
+
+    //FIXME: not using backend.stat to check target
+
+    if (path == '/' || path == '~') {
+
+        // change to root directory
+        lsPath = '/';
+    } else if (path[0] != '/') {
+
+        // path is relative to current directory
+        // append this path to current directory and normalize it 
+        lsPath = require('path').join(self.currentDirectory, path);
+
+
+    } else if (path[0] == '/') {
+
+        lsPath = require('path').resolve('/', path);
+
+
+    }
+    self.serverbackend.ls(lsPath, function (err, data) {
+
+        if (err) {
+            return cb(err);
+        }
+
+        data = data.map(function (x) {
+
+            return x.name;
+
+        });
+
+        return cb(data);
+
+    });
+
+
+
+
+};
+
+Cli.prototype.providerFriendly = function (argv) {
+
+    // Using provider as backend file system on mounted protocol 
+    // supporting cd ls cat put rmdir mkdir  commands
+    var self = this,
+        provider;
+    argv['provider'] = argv['provider'] || 'aws';
+    argv['provider'] = argv['provider'].split(':')[0];
+    argv['shareId'] = argv['provider'].split(':')[1];
+
+    provider = argv['provider'];
+
+    var serverbackend;
+
+    if (provider === 'ftp') {
+        var ftp_options = configureFtp(null, argv['ftp-config-file']);
+        serverbackend = Fio.provider(provider, ftp_options).create('prefix');
+    }
+
+    if (provider === 'ssh') {
+        var ssh_options = configureSsh(null, argv['ssh-config-file']);
+        serverbackend = Fio.provider(provider, ssh_options).create('prefix');
+    }
+
+    if (provider === 'aws') {
+
+        var aws_options = configureAws(null, argv['aws-config-file']);
+        serverbackend = Fio.provider(provider, aws_options).create('prefix');
+    }
+
+
+    self.serverbackend = serverbackend;
+
+
+};
+
+function configureFtp(Config, file) {
+
+
+    var ftp_options = {};
+    file = file || 'ftp.json';
+    Config = Config || require("../" + file);
+    var backend;
+
+    if (Config.backend.provider === 'aws') {
+        var aws_options = configureAws(Config.backend.options);
+        backend = new FolderFs(Fio.provider('aws', aws_options).create('aws'))
+
+    }
+
+    // code for other backend synthetic file systems may come here 
+
+    ftp_options.connectionString = Config.connectionString;
+    ftp_options.enableEmbeddedServer = Config.enableEmbeddedServer;
+    ftp_options.backend = backend;
+    return ftp_options;
+
+};
+
+function configureAws(Config, file) {
+    file = file || 'aws.json';
+    Config = Config || require("../" + file);
+    var aws_options = {};
+    aws_options.accessKeyId = Config.accessKeyId;
+    aws_options.secretAccessKey = Config.secretAccessKey;
+    aws_options.service = Config.service;
+    aws_options.region = Config.region;
+    aws_options.bucket = Config.bucket;
+    return aws_options;
+
+
+};
+
+
+function configureSsh(Config, file) {
+
+
+    var ssh_options = {};
+    file = file || 'ssh.json';
+    Config = Config || require("../" + file);
+
+    var backend;
+
+    if (Config.backend.provider === 'aws') {
+        var aws_options = configureAws(Config.backend.options);
+        backend = Fio.provider('aws', aws_options).create('aws')
+
+    }
+    // code for other backend synthetic file systems may come here 
+
+    ssh_options.connectionString = Config.connectionString;
+    ssh_options.enableEmbeddedServer = Config.enableEmbeddedServer;
+    ssh_options.backend = backend;
+    return ssh_options;
+
+};
+
+if (require.main.filename === __filename) {
+    // this module is the main entry point for this nodejs process
+    // compatibility for commands like node cli standalone --provider=ftp
+    new Cli();
 }
 
-cliHandler();
+
+module.exports = Cli;
--- src/vantage.js
+++ src/vantage.js
@@ -4,9 +4,165 @@
  * Suggest that npm install -g vantage is used if using the service.
  *
  */
+var Cli = require('./cli');
 var Vantage = require('vantage');
 var server = new Vantage();
 
+var banner =
 
-server.delimiter('folders~$').show();
+    "######################################################################" +
+    "#                    Welcome to folders.io                           #" +
+    "#                                                                    #" +
+    "#              All connections are monitored and recorded            #" +
+    "#      Disconnect IMMEDIATELY if you are not an authorized user      #" +
+    "######################################################################";
 
+var PORT = 3000;
+var MAX_CLIENTS = 10;
+
+server
+    .command("port")
+    .description("Gives folders.io listening port.")
+    .action(function (args, cb) {
+        this.log(PORT);
+        cb();
+    });
+
+server
+    .command("fs")
+    .description("list currently available file systems ")
+    .action(function (args, cb) {
+        this.log(['local', 'stub', 'memory', 'aws', 'hdfs', 'pkgcloud'].join('\n'));
+        cb();
+    });
+
+server
+    .command("servers")
+    .description("list currently available embedded servers")
+    .action(function (args, cb) {
+        this.log(['ftp', 'ssh', 'http'].join('\n'));
+        cb();
+    });
+
+server
+    .command("cd [DIR]")
+    .description("Change the working directory.Change the current directory to DIR.")
+    .action(function (args, cb) {
+		if (!server.cli){
+        	this.log("Error ! Mount the file system first");
+			
+		}
+		else{
+		
+			server.cli.cd(args.DIR);
+		}
+	cb();
+        
+    });
+
+server
+    .command("pwd")
+    .description("Display present working directory.")
+    .action(function (args, cb) {
+		if (!server.cli){
+        	this.log("Error ! Mount the file system first");
+			
+		}
+		else{
+		
+			this.log(server.cli.currentDirectory);
+		}
+	cb();
+        
+    });
+
+server
+    .command("ls [path]")
+    .description("List information about the FILEs (the current directory by default).")
+    .action(function (args, cb) {
+        if (!server.cli){
+        	this.log("Error ! Mount the file system first");
+			cb();
+			
+		}
+		else{
+		
+			server.cli.ls(args.path,function(data ,err){
+				
+				for (var i = 0 ; i < data.length; ++i){
+				
+					server.log(data[i]);
+				}
+				
+				cb();
+			});
+		}
+    });
+server
+    .command("mount")
+    .description("Adds a backend in union mode")
+    .action(function (args, cb) {
+        this.log("FIXME");
+        cb();
+    });
+
+server
+    .command("dump")
+    .description("Dumps json configuration file")
+    .action(function (args, cb) {
+        this.log("FIXME");
+        cb();
+    });
+
+server
+    .command("standalone [client]")
+    .option('-s, --log', 'Capture and show request logs  on terminal.')
+    .option('-c, --compress', 'Enable http compression .')
+    .option('-m, --mode <mode>', 'Starts server in live | debug mode')
+    .option('-l, --listen <port>', 'Starts standalone server on specified port.Default is 8090')
+    .option('-p, --provider <provider>', 'Specify server backend provider.Default is local.')
+	.option('--aws-config-file <config file>', 'Specify aws config file name')
+	.option('--ftp-config-file <config file>', 'Specify ftp config file name')
+	.option('--ssh-config-file <config file>', 'Specify ssh config file name')
+    .description("start folders.io embedded http server")
+    .alias('server')
+    .action(function (args, cb) {
+        args.options.client = args.client;
+        args.options._ = ['standalone'];
+        server.cli = new Cli(args.options);
+		cb();
+    });
+
+
+// FIXME: not working 	
+server
+    .command("forward")
+    .option('-f, --force', 'Force file overwrite.')
+    .option('-a, --amount <coffee>', 'Number of cups of coffee.')
+    .option('-v, --verbosity [level]', 'Sets verbosity level.')
+    .option('-A', 'Does amazing things.')
+    .description("start folders.io forwarding proxy")
+    .action(function (args, cb) {
+        console.log(args);
+        cb();
+    });
+
+server
+    .command("http-server")
+    .description("Creates a public HTTP endpoint which serves the files and folders.")
+    .action(function (args, cb) {
+        this.log(this.server._port);
+        cb();
+    });
+
+server.on('event', function () {
+
+});
+
+server.delimiter('folders~$').banner(banner)
+    .listen(PORT, function (socket) {
+
+        console.log("client connected");
+    }).show();
+
+server.parse(process.argv);
